495692954cd4edcca80eae24a34f8c8cfa7d6181
Ruslan Bihun <zuzukan76@outlook.com>
2025-11-17 11:18:52 +0100

    Create method GreetingUser, and AskNameUser

--- LessonMethods/Program.cs
+++ LessonMethods/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userName = AskNameUser();
+            string text = GreetingUser(userName);
+            Console.WriteLine(text);
+        }
+
+        /// <summary>
+        /// Greeting user by a name
+        /// </summary>
+        /// <param name="name">Name user</param>
+        /// <returns>string with greeting and name</returns>
+        static string GreetingUser(string name)
+        {
+            if (name == "Anonymous")
+            {
+                return "Hi anonymous.";
+            }
+            return $"Hello {name}! Have good Day!";
+        }
+
+
+        /// <summary>
+        /// Ask name user
+        /// </summary>
+        /// <returns>string name of user or if amty return Anonymous</returns>
+        static string AskNameUser()
+        {
+            string? name;
+
+            Console.Write("Enter you'r name: ");
+            name = Console.ReadLine();
+            if (name == "" || name == null)
+            {
+                name = "Anonymous";
+            }
+
+            return name;
         }
     }
 }
